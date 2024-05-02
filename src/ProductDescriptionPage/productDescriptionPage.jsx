e396fe7681673f7136cde87adc646059544a7c7d
Nitheesh Sudarsanan <nitheesh.sudarsanan@bounteous.com>
2024-05-02 11:50:19 +0530

    second commit

--- src/ProductDescriptionPage/productDescriptionPage.jsx
+++ src/ProductDescriptionPage/productDescriptionPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import { generateImageURL } from '../assets/imageURL';
+import { generateImageURL } from '../utils/imageURL';
 import './productDescriptionpage.css';
 import { PokemonType } from '../components/pokemonType';
 
